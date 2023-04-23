39cdd9c516ce2b48ffe547335a4843ee71495ae1
Phantasia0 <ncprog1@gmail.com>
2023-04-23 19:40:43 +0900

    plate-수정

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { legacy_createStore as createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
